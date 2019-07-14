7c0cd46f8315a9d273974a9092aaeb6c41fe7566
Daniel Sandoval <daniel@desandoval.net>
2019-07-14 09:57:46 -0400

    update api url

--- cf-cli/entrypoint.sh
+++ cf-cli/entrypoint.sh
@@ -2,7 +2,7 @@
 
 
 if [ -n "$CF_USERNAME" ] && [ -n "$CF_PASSWORD" ]; then
-  cf login -u $CF_USERNAME -p $CF_PASSWORD
+  cf login -a "https://api.run.pivotal.io" -u $CF_USERNAME -p $CF_PASSWORD
 fi
 
 sh -c "cf $*"
