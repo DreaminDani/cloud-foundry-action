af541fb24bd8302489dffa45b784b4bce90bf4a1
Daniel Sandoval <daniel@desandoval.net>
2019-07-14 10:17:36 -0400

    no export

--- cf-cli/entrypoint.sh
+++ cf-cli/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -l
 
 if [ -n "$CF_USERNAME" ] && [ -n "$CF_PASSWORD" ]; then
-  export CF_USERNAME=$CF_USERNAME
-  export CF_PASSWORD=$CF_PASSWORD
+  CF_USERNAME=$CF_USERNAME
+  CF_PASSWORD=$CF_PASSWORD
 
   cf api "https://api.run.pivotal.io"
   cf auth
