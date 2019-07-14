0e4e338837d23c9efd8e5c39d65fca9bdc326669
Daniel Sandoval <daniel@desandoval.net>
2019-07-14 10:20:53 -0400

    revert

--- cf-cli/entrypoint.sh
+++ cf-cli/entrypoint.sh
@@ -1,11 +1,9 @@
 #!/bin/sh -l
 
-if [ -n "$CF_USERNAME" ] && [ -n "$CF_PASSWORD" ]; then
-  CF_USERNAME=$CF_USERNAME
-  CF_PASSWORD=$CF_PASSWORD
 
+if [ -n "$CF_USERNAME" ] && [ -n "$CF_PASSWORD" ]; then
   cf api "https://api.run.pivotal.io"
-  cf auth
+  cf auth "$CF_USERNAME" "$CF_PASSWORD"
 fi
 
 sh -c "cf $*"
