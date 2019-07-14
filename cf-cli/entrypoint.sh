33991d3e40845008ee70b7f6cc0cd8bc30920345
Daniel Sandoval <daniel@desandoval.net>
2019-07-14 10:14:19 -0400

    try with secrets as environment

--- cf-cli/entrypoint.sh
+++ cf-cli/entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/sh -l
 
-
 if [ -n "$CF_USERNAME" ] && [ -n "$CF_PASSWORD" ]; then
+  export CF_USERNAME=$CF_USERNAME
+  export CF_PASSWORD=$CF_PASSWORD
+
   cf api "https://api.run.pivotal.io"
-  cf auth "$CF_USERNAME" "$CF_PASSWORD"
+  cf auth
 fi
 
 sh -c "cf $*"
